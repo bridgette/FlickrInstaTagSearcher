3e2d47c0e0384ea879fc13a5960de3d552ba61cc
Patrick McNally <patito@pom-nat-84-9.pomona.edu>
2013-09-18 13:56:15 -0700

    Initial Commit

---
+++ TabBarExampleTests/TabBarExampleTests.h
@@ -0,0 +1,13 @@
+//
+//  TabBarExampleTests.h
+//  TabBarExampleTests
+//
+//  Created by Patrick McNally on 9/18/13.
+//  Copyright (c) 2013 Pomona College. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface TabBarExampleTests : SenTestCase
+
+@end
---
+++ TabBarExampleTests/TabBarExampleTests.m
@@ -0,0 +1,32 @@
+//
+//  TabBarExampleTests.m
+//  TabBarExampleTests
+//
+//  Created by Patrick McNally on 9/18/13.
+//  Copyright (c) 2013 Pomona College. All rights reserved.
+//
+
+#import "TabBarExampleTests.h"
+
+@implementation TabBarExampleTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in TabBarExampleTests");
+}
+
+@end
